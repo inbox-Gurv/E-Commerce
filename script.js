680eb828904cfcc50917737ef6318b26bf093ef3
inbox-Gurv <gauravkumarkr12804@gmail.com>
2026-02-25 17:41:09 +0530

    Change navbaar

--- script.js
+++ script.js
@@ -65,3 +65,79 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
         "Thank you! Your message has been sent successfully.";
     this.reset();
 });
+
+
+
+
+
+
+let cart = {};
+
+function addToCart(name, price){
+
+    if(cart[name]){
+        cart[name].qty += 1;
+    } else {
+        cart[name] = {
+            price: price,
+            qty: 1
+        };
+    }
+
+    renderCart();
+}
+
+function increaseQty(name){
+    cart[name].qty += 1;
+    renderCart();
+}
+
+function decreaseQty(name){
+    cart[name].qty -= 1;
+
+    if(cart[name].qty <= 0){
+        delete cart[name];
+    }
+
+    renderCart();
+}
+
+function renderCart(){
+
+    let cartItemsDiv = document.getElementById("cartItems");
+    let cartCount = 0;
+    let total = 0;
+
+    cartItemsDiv.innerHTML = "";
+
+    for(let item in cart){
+
+        cartCount += cart[item].qty;
+        total += cart[item].price * cart[item].qty;
+
+        cartItemsDiv.innerHTML += `
+        <div class="cart-item">
+            <p>${item}</p>
+            <div class="qty-controls">
+                <button onclick="decreaseQty('${item}')">−</button>
+                <span>${cart[item].qty}</span>
+                <button onclick="increaseQty('${item}')">+</button>
+            </div>
+            <p>₹${cart[item].price * cart[item].qty}</p>
+        </div>
+        `;
+    }
+
+    document.getElementById("cartCount").innerText = cartCount;
+    document.getElementById("total").innerText = total;
+}
+
+function toggleCart(){
+    document.getElementById("cartSidebar").classList.toggle("active");
+}
+
+
+
+function toggleMenu(){
+document.getElementById("navMenu").classList.toggle("active");
+}
